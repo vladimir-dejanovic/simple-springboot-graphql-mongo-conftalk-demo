e59aa1a315b9478142a280f01ca351eb164e99ec
Vladimir Dejanovic <ed.wong.iv@gmail.com>
2017-11-05 16:49:22 +0100

    init code commit

---
+++ src/main/java/xyz/itshark/restdemo/simplespringgraphqlmongoconftalkdemo/SimpleSpringGraphqlMongoConftalkDemoApplication.java
@@ -0,0 +1,12 @@
+package xyz.itshark.restdemo.simplespringgraphqlmongoconftalkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringGraphqlMongoConftalkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringGraphqlMongoConftalkDemoApplication.class, args);
+	}
+}
